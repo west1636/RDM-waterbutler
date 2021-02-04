14bf8f2d1dce88055a46db66bf6c124c9f0e6c40
ivis-miyazawa <tatsuaki.miyazawa@ivis.co.jp>
2021-02-04 09:59:31 +0900

    Add googledriveinstitutions

---
+++ waterbutler/providers/googledriveinstitutions/settings.py
@@ -0,0 +1,8 @@
+from waterbutler import settings
+
+config = settings.child('GOOGLEDRIVEINSTITUTIONS_PROVIDER_CONFIG')
+
+
+BASE_URL = config.get('BASE_URL', 'https://www.googleapis.com/drive/v3')
+BASE_UPLOAD_URL = config.get('BASE_UPLOAD_URL', 'https://www.googleapis.com/upload/drive/v3')
+DRIVE_IGNORE_VERSION = config.get('DRIVE_IGNORE_VERSION', '0000000000000000000000000000000000000')
